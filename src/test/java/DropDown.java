203248708113319d97f9760a4f3f3a2c9709d4c7
Kristina Popowa <popowa.kri2011@yandex.ru>
2021-11-22 23:55:52 +0300

    Implemented Test3

--- src/test/java/DropDown.java
+++ src/test/java/DropDown.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,11 +20,26 @@ public class DropDown {
         WebElement clickDropDownButton = driver.findElement(By.xpath("//a[@href='/dropdown']"));
         clickDropDownButton.click();
 
-        List<WebElement> allElements = driver.findElements(By.xpath("//select"));
-        Assert.assertEquals(allElements.size(), 2);
+        List<WebElement> allElements = driver.findElements(By.xpath("//select/option"));
+        Assert.assertEquals(allElements.size(), 3);
 
         WebElement choseOptionButton = driver.findElement(By.xpath("//select[@id='dropdown']"));
         choseOptionButton.click();
 
+        WebElement testDropDown = driver.findElement(By.xpath("//select"));
+        Select dropdown = new Select(testDropDown);
+        dropdown.selectByIndex(1);
+
+        WebElement option1IsSelected = driver.findElement(By.xpath("//select/option [@value='1' and (@selected)]"));
+        Assert.assertEquals(option1IsSelected.isDisplayed(),true);
+
+        dropdown.selectByIndex(2);
+
+        WebElement option2IsSelected = driver.findElement(By.xpath("//select/option [@value='2' and (@selected)]"));
+        Assert.assertEquals(option2IsSelected.isDisplayed(),true);
+
+        driver.quit();
+
     }
+
 }
